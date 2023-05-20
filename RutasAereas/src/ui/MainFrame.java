c517281b000904d1c864e809dd986920f1d92511
Kirbi Huerta <kirbyxavier2@outlook.com>
2023-05-19 23:57:21 -0600

    Mapa integrado en panel, y ubicando el panel de info a la derecha del mapa

--- RutasAereas/src/ui/MainFrame.java
+++ RutasAereas/src/ui/MainFrame.java
@@ -1,18 +1,19 @@
 package ui;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
+import java.awt.Graphics;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainFrame extends JFrame{
     
     public MainFrame(){
+        
         initComponents();
+        //this.setContentPane(mapa);
     }
 
     public void initComponents(){
@@ -20,17 +21,18 @@ public class MainFrame extends JFrame{
 
         panelPrincipal = new JPanel();
         panelPrincipal.setLayout(null);
-        panelPrincipal.setBackground(Color.ORANGE);
+        panelPrincipal.setBackground(Color.decode("#F2F0EC"));
         panelPrincipal.setVisible(true);
 
         panelInfo = new JPanel();
         panelInfo.setBackground(Color.BLACK);
-//        panelMapa.setSize(200, 600);
+        panelInfo.setSize(250, 460);
+        panelInfo.setLocation(900,0);
         panelInfo.setVisible(true);
 
-        panelMapa = new JPanel();
+        panelMapa = new MapaPanel();
         panelMapa.setBackground(Color.green);
-        panelMapa.setSize(800, 600);
+        panelMapa.setSize(900, 460);
         panelMapa.setVisible(true);
 
         panelPrincipal.add(panelMapa, LEFT_ALIGNMENT);
@@ -43,4 +45,17 @@ public class MainFrame extends JFrame{
     private JPanel panelPrincipal;
     private JPanel panelInfo;
     private JPanel panelMapa;
+    private MapaPanel mapa = new MapaPanel();
+
+    class MapaPanel extends JPanel{
+        private Image mapa;
+
+        @Override
+        public void paint(Graphics g){
+            mapa = new ImageIcon(getClass().getResource("/images/map.png")).getImage();
+            g.drawImage(mapa, 0, 0, getWidth(), getHeight(), this);
+            setOpaque(false);
+            super.paint(g);
+        }
+    }
 }
