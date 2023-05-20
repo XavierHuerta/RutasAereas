9719410eb28b0ac7c578671196d57f11d226acde
Kirbi Huerta <kirbyxavier2@outlook.com>
2023-05-19 19:51:50 -0600

    Creacion de la carpeta ui y del archivo de Frame principal

--- RutasAereas/src/App.java
+++ RutasAereas/src/App.java
@@ -1,5 +1,14 @@
+import javax.swing.JFrame;
+
+import ui.MainFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        MainFrame mFrame = new MainFrame();
+        mFrame.setSize(800, 600);
+        mFrame.setLocationRelativeTo(null);
+        mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mFrame.setResizable(false);
+        mFrame.setVisible(true);
     }
 }
---
+++ RutasAereas/src/ui/MainFrame.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+    
+    public MainFrame(){
+
+    }
+
+    public void initComponents(){
+
+    }
+}
