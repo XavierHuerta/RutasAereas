5a87a9404abfc3bbdc84368b131812eef1e7b27c
Kirbi Huerta <kirbyxavier2@outlook.com>
2023-05-20 13:11:06 -0600

    intento de agregar marcadores al mapa n1

--- RutasAereas/src/ui/MainFrame.java
+++ RutasAereas/src/ui/MainFrame.java
@@ -2,10 +2,15 @@ package ui;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.GridBagLayout;
 import java.awt.Image;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MainFrame extends JFrame{
@@ -25,27 +30,45 @@ public class MainFrame extends JFrame{
         panelPrincipal.setVisible(true);
 
         panelInfo = new JPanel();
-        panelInfo.setBackground(Color.BLACK);
+        panelInfo.setBackground(Color.white);
         panelInfo.setSize(250, 560);
         panelInfo.setLocation(1000,0);
         panelInfo.setVisible(true);
 
         panelMapa = new MapaPanel();
-        panelMapa.setBackground(Color.green);
+        panelMapa.setLayout(null);
         panelMapa.setSize(1000, 560);
         panelMapa.setVisible(true);
 
+        bandera = new PuntoBandera();
+        bandera.setText("Aqui");
+        bandera.setForeground(Color.CYAN);
+        bandera.setBounds(100, 100, 100, 50);
+
+        // JButton boton = new JButton("Haz clic");
+        // boton.setBounds(50, 50, 100, 30);
+        
+        
+
+        //Elementos del panel principal
         panelPrincipal.add(panelMapa, LEFT_ALIGNMENT);
         panelPrincipal.add(panelInfo, RIGHT_ALIGNMENT);
 
+        panelInfo.add(bandera);
+
+        //Elementos del panel mapa
+        panelMapa.add(bandera);
+        //panelMapa.add(boton);
+
+        //Elementos del Frame
         add(panelPrincipal);
-        //add(panelInfo, FlowLayout.LEFT);
     }
 
     private JPanel panelPrincipal;
     private JPanel panelInfo;
     private JPanel panelMapa;
-    //private MapaPanel mapa = new MapaPanel();
+    private JLabel bandera;
+    
 
     class MapaPanel extends JPanel{
         private Image mapa;
@@ -56,6 +79,15 @@ public class MainFrame extends JFrame{
             g.drawImage(mapa, 0, 0, getWidth(), getHeight(), this);
             setOpaque(false);
             super.paint(g);
+        }
+    }
+
+    class PuntoBandera extends JLabel{
+        private ImageIcon bandera;
+        public PuntoBandera(){
+            bandera = new ImageIcon("/images/map.png");
+
+            setIcon(bandera);
         }
     }
 }
