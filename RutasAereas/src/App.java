e46392e81e6bf54a1ecebe06bcce3ebf2ca68111
Kirbi Huerta <kirbyxavier2@outlook.com>
2023-05-19 22:26:03 -0600

    Solucion al tamaño de los paneles

--- RutasAereas/src/App.java
+++ RutasAereas/src/App.java
@@ -5,7 +5,7 @@ import ui.MainFrame;
 public class App {
     public static void main(String[] args) throws Exception {
         MainFrame mFrame = new MainFrame();
-        mFrame.setSize(800, 600);
+        mFrame.setSize(1150, 600);
         mFrame.setLocationRelativeTo(null);
         mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mFrame.setResizable(false);
--- RutasAereas/src/ui/MainFrame.java
+++ RutasAereas/src/ui/MainFrame.java
@@ -1,14 +1,46 @@
 package ui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame{
     
     public MainFrame(){
-
+        initComponents();
     }
 
     public void initComponents(){
+        //setLayout(new FlowLayout());
 
+        panelPrincipal = new JPanel();
+        panelPrincipal.setLayout(null);
+        panelPrincipal.setBackground(Color.ORANGE);
+        panelPrincipal.setVisible(true);
+
+        panelInfo = new JPanel();
+        panelInfo.setBackground(Color.BLACK);
+//        panelMapa.setSize(200, 600);
+        panelInfo.setVisible(true);
+
+        panelMapa = new JPanel();
+        panelMapa.setBackground(Color.green);
+        panelMapa.setSize(800, 600);
+        panelMapa.setVisible(true);
+
+        panelPrincipal.add(panelMapa, LEFT_ALIGNMENT);
+        panelPrincipal.add(panelInfo, RIGHT_ALIGNMENT);
+
+        add(panelPrincipal);
+        //add(panelInfo, FlowLayout.LEFT);
     }
+
+    private JPanel panelPrincipal;
+    private JPanel panelInfo;
+    private JPanel panelMapa;
 }
