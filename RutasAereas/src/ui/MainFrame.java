492ce2e241d74dd7a3e5321478425558bc005ac6
Kirbi Huerta <kirbyxavier2@outlook.com>
2023-05-21 13:58:44 -0600

    insertar iconos logrado :D

--- RutasAereas/src/ui/MainFrame.java
+++ RutasAereas/src/ui/MainFrame.java
@@ -1,10 +1,12 @@
 package ui;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 
+import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -12,6 +14,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 public class MainFrame extends JFrame{
     
@@ -22,54 +25,76 @@ public class MainFrame extends JFrame{
     }
 
     public void initComponents(){
-        //setLayout(new FlowLayout());
+        setLayout(null);
 
-        panelPrincipal = new JPanel();
-        panelPrincipal.setLayout(null);
-        panelPrincipal.setBackground(Color.decode("#F2F0EC"));
-        panelPrincipal.setVisible(true);
-
+        //Propiedades del Panel Info
         panelInfo = new JPanel();
+        panelInfo.setLayout(new FlowLayout());
         panelInfo.setBackground(Color.white);
         panelInfo.setSize(250, 560);
         panelInfo.setLocation(1000,0);
         panelInfo.setVisible(true);
 
+        //Propiedades del Panel Mapa
         panelMapa = new MapaPanel();
         panelMapa.setLayout(null);
+        panelMapa.setBorder(new LineBorder(Color.DARK_GRAY));
         panelMapa.setSize(1000, 560);
         panelMapa.setVisible(true);
 
+        /*Creacion de las etiquetas destino */
+        //Etiquetas destino
         bandera = new PuntoBandera();
         bandera.setText("Aqui");
         bandera.setForeground(Color.CYAN);
         bandera.setBounds(100, 100, 100, 50);
+        
+        marca = new JLabel(new ImageIcon("src/images/marcador.png"));
+        marca.setBackground(Color.BLACK);
+        marca.setBounds(40, 40, 30, 30);
 
-        // JButton boton = new JButton("Haz clic");
+        destino = new JLabel(new ImageIcon("src/images/bandera.png"));
+        destino.setBounds(150, 150, 26, 26);
+        //Etiqueta para paris
+        paris = new PuntoBandera();
+        paris.setBounds(780, 280, 30, 30);
+
+
+        JButton boton = new JButton("Haz clic");
         // boton.setBounds(50, 50, 100, 30);
         
         
 
-        //Elementos del panel principal
-        panelPrincipal.add(panelMapa, LEFT_ALIGNMENT);
-        panelPrincipal.add(panelInfo, RIGHT_ALIGNMENT);
 
-        panelInfo.add(bandera);
+        panelInfo.add(boton);
 
-        //Elementos del panel mapa
+        //adicion de los marcadores al panelMapa
         panelMapa.add(bandera);
-        //panelMapa.add(boton);
-
-        //Elementos del Frame
-        add(panelPrincipal);
+        panelMapa.add(marca);
+        panelMapa.add(destino);
+        panelMapa.add(paris);
+        
+ 
+        add(panelMapa);
+        add(panelInfo);
     }
 
+    /*Elementos del Panel Mapa */
     private JPanel panelPrincipal;
     private JPanel panelInfo;
     private JPanel panelMapa;
+    //Etiquetas destino
     private JLabel bandera;
+    private JLabel marca;
+    private JLabel destino;
+    private JLabel paris;
+
+    /*Elementos del panel Info */
+    
     
 
+    /*CLASES INTERNAS -------------------------------------------------------------------------------- */
+    //Clase para hacer el panel con fondo de mapa
     class MapaPanel extends JPanel{
         private Image mapa;
 
@@ -82,11 +107,11 @@ public class MainFrame extends JFrame{
         }
     }
 
+    //Clase para hacer etiquetas con un mismo icon
     class PuntoBandera extends JLabel{
         private ImageIcon bandera;
         public PuntoBandera(){
-            bandera = new ImageIcon("/images/map.png");
-
+            bandera = new ImageIcon("src/images/bandera.png");
             setIcon(bandera);
         }
     }
