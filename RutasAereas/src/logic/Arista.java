377c11e46bc6f5ff919bb934c0974de99d96893d
Kirbi Huerta <kirbyxavier2@outlook.com>
2023-05-28 02:14:58 -0600

    inicio de la animacion

--- RutasAereas/src/logic/Arista.java
+++ RutasAereas/src/logic/Arista.java
@@ -13,13 +13,14 @@ public class Arista {
     private double peso; //Costo
     private Point2D pi, pf; //Pi = punto inicial (x,y) ; pf = punto dinal (x,y)
     private Color color;
-    private Line2D linea; //Skin linea
+    //private Line2D linea; //Skin linea
+    private Path2D curva;
 
     /* CONSTRUCTORES */
     public Arista(Point2D pi, Point2D pf){
         this(pi, pf, 0);
         color = Color.RED;
-        linea = new Line2D.Double(pi,pf);
+        //linea = new Line2D.Double(pi,pf);
         
     }
     public Arista (Point2D pi, Point2D pf, double peso){
@@ -27,34 +28,36 @@ public class Arista {
         this.pi = pi;
         this.pf = pf;
         color = Color.BLACK;
-        linea = new Line2D.Double(pi,pf);
+        //linea = new Line2D.Double(pi,pf);
     }
  
     /* METODOS ----------------------------------------------------------------------------------- */
     //Dibuja la linea
-    public void dibujar(Graphics2D g2){
-        BasicStroke grosor1 = new BasicStroke(2);//Grosor de la linea
-        g2.setStroke(grosor1);
-        g2.setPaint(color);
-        g2.draw(linea);
-        g2.setPaint(Color.BLACK);//Color del Peso
+    // public void dibujar(Graphics2D g2){
+    //     BasicStroke grosor1 = new BasicStroke(2);//Grosor de la linea
+    //     g2.setStroke(grosor1);
+    //     g2.setPaint(color);
+    //     g2.draw(linea);
+    //     g2.setPaint(Color.BLACK);//Color del Peso
 
-        //mostrar peso entre arista
-        //g2.drawString(String.valueOf(peso), (float) ((pi.getX() + pf.getX()) / 2), (float) ((pi.getY() + pf.getY()) / 2) );
-    }
+    //     //mostrar peso entre arista
+    //     //g2.drawString(String.valueOf(peso), (float) ((pi.getX() + pf.getX()) / 2), (float) ((pi.getY() + pf.getY()) / 2) );
+    // }
 
     public void pintar(Graphics g){
         g.setColor(Color.red);
 
         //Calcular punto medio
 
-        Path2D curva = new Path2D.Double();
-        curva.moveTo(pi.getX(), pi.getY());
-        curva.curveTo(pi.getX(), pi.getY(), (pi.getX() + pf.getX()) / 2, ((pi.getY() + pf.getY()) / 2) - 50, pf.getX(), pf.getY());
+        curva = new Path2D.Double();//Crea un path que es como un pincel
+        curva.moveTo(pi.getX(), pi.getY());//mueve el pincel a la posicion inicial
+        curva.curveTo(pi.getX(), pi.getY(), (pi.getX() + pf.getX()) / 2, 
+                    ((pi.getY() + pf.getY()) / 2) - 50, pf.getX(), pf.getY());//Dibuja la curva en puntos especificados
 
+        //Modifica la linea de la curva y la hace punteada
         float[] dashPattern = {5, 5};
         Graphics2D g2d = (Graphics2D) g;
-        //g2d.setStroke(new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1, dashPattern, 0));
+        g2d.setStroke(new BasicStroke(2, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1, dashPattern, 0));
         g2d.draw(curva);
 
     }
@@ -63,12 +66,13 @@ public class Arista {
     public void setPeso(){
         peso = Double.parseDouble(JOptionPane.showInputDialog("Indica el peso:"));
     }
-
     public void setColor(Color color){
         this.color = color;
     }
-
     public double getPeso(){
         return peso;
+    }
+    public Path2D getPath2d(){
+        return curva;
     }
 }
